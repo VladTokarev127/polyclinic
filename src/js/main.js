45d337c061daa213bb8eb01af767015da25f28b1
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-14 19:02:34 +0500

    header done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
- // JS - код
+	if($(window).width() < 992) {
+		$('.menu-item-has-children > a').click(function(e) {
+			e.preventDefault();
+			$(this).next().slideToggle(300);
+		})
+	} else {
+		$('.menu-item-has-children').hover(function() {
+			$(this).find('ul').stop().slideDown(300);
+		}, function() {
+			$(this).find('ul').stop().slideUp(300);
+		})
+	}
+
+	$('.header__btn').click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass('is-active');
+		$('.header__mobile').slideToggle(300);
+		$('body').toggleClass('is-fixed');
+	})
 
 });
